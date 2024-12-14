fdabe8215cd983c283c91bd214e64c4a7f123e5b
sodiki366 <mark1704kos@gmail.com>
2024-12-14 15:59:34 +0300

    добавили уменьшение скорости на воде

--- 3 ,23 October 2024/Tank.py
+++ 3 ,23 October 2024/Tank.py
@@ -36,15 +36,28 @@ class Tank:
         if self.__y < 0:
             self.__y = 0
 
+        self.__usual_speed = speed
+        self.__water_speed = speed / 2
+
         self.__create()
         self.right()
 
+    def __set_usual_speed(self):
+        self.__speed = self.__usual_speed
+    def __set_water_speed(self):
+        self.__speed = self.__water_speed
+
     def __check_map_collision(self):
-        result = self.__hitbox.check_map_collision()
+        details = {}
+        self.__set_usual_speed()
+        result = self.__hitbox.check_map_collision(details)
         if result:
-            self.__undo_move()
-            if self.__bot:
-                self.__AI_change_orientation()
+            if details['block'] == world.WATER:
+                self.__set_water_speed()
+            else:
+                self.__undo_move()
+                if self.__bot:
+                     self.__AI_change_orientation()
 
     def set_target(self, target):
         self.__target = target
